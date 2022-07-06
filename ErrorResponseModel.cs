0ca563dc26d8d4b571d9e48724d8234cda566097
AbhiKumar60502 <33263723+AbhiKumar60502@users.noreply.github.com>
2022-07-06 18:54:39 -0500

    Add files via upload

---
+++ ErrorResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Shm.Services.Functions
+{
+    public class ErrorResponseModel
+    {
+        public ErrorResponseModel(Exception exception)
+        {
+            ErrorMessage = exception.Message;
+        }
+
+        public string ErrorMessage { get; }
+    }
+}
