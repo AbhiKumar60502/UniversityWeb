8d854c55410509ae36ebaafcf92c4f1c5ba694a9
AbhiKumar60502 <33263723+AbhiKumar60502@users.noreply.github.com>
2022-07-06 18:57:39 -0500

    Add files via upload

---
+++ DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Shm.Services.Types
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
