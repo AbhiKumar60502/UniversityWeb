c8529e42a1b53316c7e01d1b5ee0cc5a9153f126
AbhiKumar60502 <33263723+AbhiKumar60502@users.noreply.github.com>
2022-07-19 15:03:27 -0500

    Add files via upload

---
+++ CommonExceptionStrings.cs
@@ -0,0 +1,11 @@
+﻿namespace Shm.Servicing.Handlers
+{
+    public class CommonExceptionStrings
+    {
+        public const string CannotBeLessThanZero = "{0} cannot be less than or equal to zero";
+        public const string ProblemUpdating = "Problem updating {0} with ID {1}";
+        public const string CouldNotFindWorkOrderWithId = "Could not find a work order with Id {0} for marina {1}";
+        public const string NotFound = "Could not find a {0} with Id {1}";
+        public const string CouldNotCreate = "Inserting {0} did not return the inserted primary key";
+    }
+}
